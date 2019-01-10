7839a0c392f20c8e08feb8d3cac447f3d948baf4
Gregory Kallai <gerg.kallai@gmail.com>
2019-01-10 21:58:41 +0100

    Adds the getcharacter method and the url hard coded to the first instance

--- src/app/character.service.ts
+++ src/app/character.service.ts
@@ -3,5 +3,12 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Character {
+
+  private swUrl = 'https://swapi.co/api/people/1';
+
   constructor(private http: HttpClient) {}
+
+  getCharacter() {
+    return this.http.get(this.swUrl);
+  }
 }
